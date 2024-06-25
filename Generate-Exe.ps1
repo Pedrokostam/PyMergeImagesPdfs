c1d060a80a2370102f53dc65be825971b4499f73
Pedrokostam <74862338+Pedrokostam@users.noreply.github.com>
2024-06-25 18:52:07 +0200

    removed old code in generate-exe

--- Generate-Exe.ps1
+++ Generate-Exe.ps1
@@ -3,16 +3,4 @@ if ((Test-Path .venv) -and -not $env:VIRTUAL_ENV){
     . ./.venv/Scripts/activate.ps1
 }
 python ./cx_freeze_generate_exe.py build
-
-# Remove-Item .\dist -ea silentlycontinue -Recurse
-# pyinstaller --noconfirm `
-#     --onedir --console `
-#     --icon "$psscriptroot\icon.ico" `
-#     --add-data=language.json:. `
-#     --add-data=config.toml:. `
-#     --add-data 'implementation:implementation' `
-#     --paths . `
-#     --clean `
-#     "$psscriptroot\merge_documents.py" 
-
 Pop-Location
